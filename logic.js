740599353bda5a39b695b572341583d72f80472a
Utkarsh Seksaria <utkarshseksaria@gmail.comexitexit()exitgit config --global user.email utkarshseksaria@gmail.com>
2021-04-16 01:24:20 +0530

    Fix Firefox problem

--- logic.js
+++ logic.js
@@ -1,11 +1,15 @@
 var value = document.getElementById('zero');
-window.scrollBy(0,1e10);
+window.scrollBy(0,1000000);
 var maxi= window.pageYOffset;
-window.scrollBy(0,-1e10);
+window.scrollBy(0,-100000);
+// console.log("maxi is "); 
+// console.log('hello');
 document.addEventListener('scroll',function(){
-    console.log('scrolled');
+    // console.log('scrolled ');
+    console.log(maxi);
     var cur = window.pageYOffset;
     var per = (cur/maxi)*100;
     per = Math.floor(per);
     value.innerHTML = per + "";
+    console.log
 })
